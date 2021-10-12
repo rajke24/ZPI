5e05d13ac3d93fa8732d65e17ede0391e984321d
JerryAndrzejewski <jerry.andrzejewski@gdsoft.info>
2021-10-12 20:21:02 +0200

    Add oAuth authorization

---
+++ backend/app/models/user.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+
+  class << self
+    def authenticate(email, password)
+      user = User.find_by_email(email)
+
+      if user&.authenticate(password)
+        user
+      else
+        raise Doorkeeper::Errors::InvalidGrantReuse
+      end
+    end
+  end
+end
