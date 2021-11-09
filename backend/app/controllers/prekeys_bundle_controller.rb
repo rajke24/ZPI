df9b8e99b1456bcc0f730835d09bc1ca6dc29516
norbert <norbertn15@gmail.com>
2021-11-09 18:23:55 +0100

    Add prekeys bundle controller

---
+++ backend/app/controllers/prekeys_bundle_controller.rb
@@ -0,0 +1,26 @@
+class PrekeysBundleController < ApplicationController
+  def save_prekeys_bundle
+    prekeys_bundle_info = {
+      identity_key: message_params[:identityKey],
+      prekeys: message_params[:preKeys],
+      signed_prekey: message_params[:signedPreKey]
+    }
+
+    current_user.update(prekeys_bundle_info)
+  end
+
+  def send_prekeys_bundle_info
+    to_pass = {
+      identity_key: current_user.identity_key,
+      signed_key: current_user.signed_prekey,
+      prekey: current_user.prekeys[0]
+    }
+    # TODO remove taken prekey
+  end
+
+  PREKEY_BUNDLE_PARAMS = [:identityKey, :preKeys, :signedPreKey]
+
+  def message_params
+    params.permit(PREKEY_BUNDLE_PARAMS)
+  end
+end
