663964980b546ea79ba5ee48b1f73da5e6f50605
JerryAndrzejewski <jerry.andrzejewski@gdsoft.info>
2021-11-22 20:24:32 +0100

    Add invitation feedback

--- frontend/src/components/navigation/Topbar.js
+++ frontend/src/components/navigation/Topbar.js
@@ -8,6 +8,7 @@ import {post} from "../../shared/ApiClientBuilder";
 import * as Yup from "yup";
 import {addValidation} from "../../common/form/FromSchemaBuilder";
 import {doSave} from "../../common/form/FormHelpers";
+import FormFeedback from "../../common/form/FormFeedback";
 
 const validationSchema = Yup.object().shape({
     invitee_email: addValidation({email: true, required: true, unique: '/invitations/check'}),
@@ -16,11 +17,12 @@ const validationSchema = Yup.object().shape({
 const Topbar = () => {
     const {pathname} = useLocation();
     const [showInviteInput, setShowInviteInput] = useState(false);
+    const [invitationSent, setInvitationSent] = useState(false);
     const profile = useSelector(state => state.persistentState.profile);
     const title = pathname.split('/')[1]
 
     const formik = useDefaultFormik({
-        initialValues: {},
+        initialValues: {invitee_email: ''},
         validationSchema
     })
 
@@ -32,8 +34,23 @@ const Topbar = () => {
         event.preventDefault();
 
         doSave(formik, (values) => {
-            actions.sendInvitation({invitee_email: values.invitee_email})
+            actions.sendInvitation({invitee_email: values.invitee_email}, () => {
+                setShowInviteInput(false);
+                setInvitationSent(true);
+                formik.setFieldValue('invitee_email', '')
+                setTimeout(() => setInvitationSent(false), 5000)
+            })
         })
+    }
+
+    const inviteButtonFunction = (e) => {
+        if (showInviteInput && formik.values.invitee_email.length === 0) {
+            setShowInviteInput(false);
+            formik.setErrors({})
+        }
+        else if (!showInviteInput)
+            setShowInviteInput(true);
+        else sendInvitation(e)
     }
 
     return (
@@ -49,9 +66,10 @@ const Topbar = () => {
                         }
                     ], formik, validationSchema)}
                 </form>}
-                <button className='invite-btn' onClick={showInviteInput ? sendInvitation : () => setShowInviteInput(true)}>Invite new friend</button>
+                {invitationSent && <FormFeedback success>Invitation sent!</FormFeedback>}
+                <button className='invite-btn' onClick={inviteButtonFunction}>{!showInviteInput || formik.values.invitee_email ? 'Invite new friend' : 'Hide'}</button>
                 <span>{profile.email}</span>
-                <Avatar color='#000000' name='Jarek A'/>
+                <Avatar color='#000000' name={profile.email}/>
             </div>
         </div>
     );
