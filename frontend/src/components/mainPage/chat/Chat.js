80dc3ddec8e6349ba4119581b0d3e8a9091c64fd
Jacek <jacek.szelewski@gmail.com>
2021-11-25 12:39:46 +0100

    Add decryption of whisper-type messages - FE

--- frontend/src/components/mainPage/chat/Chat.js
+++ frontend/src/components/mainPage/chat/Chat.js
@@ -23,13 +23,26 @@ const Chat = () => {
     useEffect(() => {
         MessagesChannel.received = data => {
             const message = {...data.message, message_type: data.message.sender_id === profile.id ? 'sent' : 'received'}
-            const otherUserId = profile.id === message.receiver_id ? message.sender_id : message.receiver_id
-            const sessionCipher = new window.libsignal.SessionCipher(myProtocolStore, new window.libsignal.SignalProtocolAddress(otherUserId.toString(), 1));
-            sessionCipher.decryptPreKeyWhisperMessage(JSON.parse(message.content).body, 'binary').then(newPlaintext => {
-                message.content = new TextDecoder('utf-8').decode(newPlaintext);
-                db.conversations.where({sender_id: profile.id, 'receiver.id': otherUserId}).modify(c => c.messages.push(message))
-            });
+            if(message.receiver_id !== profile.id) {
+                console.log("Received message where I am not a receiver so skipped it");
+                return;
+            }
+            const sessionCipher = new window.libsignal.SessionCipher(myProtocolStore, new window.libsignal.SignalProtocolAddress(message.sender_id.toString(), 1));
+            const ciphertext = JSON.parse(message.content);
 
+            let decryptionMethod;
+            if(ciphertext.type === 3) {
+                decryptionMethod = sessionCipher.decryptPreKeyWhisperMessage;
+            } else if(ciphertext.type === 1) {
+                decryptionMethod = sessionCipher.decryptWhisperMessage;
+            } else {
+                console.error("Unknown ciphertext type! Type: " + ciphertext.type);
+                return;
+            }
+            decryptionMethod(ciphertext.body, 'binary').then(newPlaintext => {
+                message.content = new TextDecoder('utf-8').decode(newPlaintext);
+                db.conversations.where({sender_id: profile.id, 'receiver.id': message.sender_id}).modify(c => c.messages.push(message));
+            });
         }
     }, [params.name])
 
@@ -41,18 +54,28 @@ const Chat = () => {
         return new Uint8Array(JSON.parse(string)).buffer;
     }
 
-
-
     const actions = {
         sendMessage: () => {
             const sessionCipher = new window.libsignal.SessionCipher(myProtocolStore, new window.libsignal.SignalProtocolAddress(conversation.receiver.id.toString(), 1));
             sessionCipher.encrypt(currentMessage).then(ciphertext => {
-               save('messages/save_message', 'POST', {
-                   content: JSON.stringify(ciphertext),
-                   receiver_id: conversation.receiver.id,
-                   sent_at: new Date(),
-                   type: 'type'
-               }, () => setCurrentMessage(''))
+                let date = new Date()
+                save('messages/save_message', 'POST', {
+                    content: JSON.stringify(ciphertext),
+                    receiver_id: conversation.receiver.id,
+                    sent_at: date,
+                    type: 'type'
+               }, (params) => {
+                    let sent_message = {
+                        content: currentMessage,
+                        id: params.message_id,
+                        message_type: 'sent',
+                        receiver_id: conversation.receiver.id,
+                        sender_id: profile.id,
+                        send_at: date
+                    };
+                    db.conversations.where({sender_id: profile.id, 'receiver.id': conversation.receiver.id}).modify(c => c.messages.push(sent_message));
+                    setCurrentMessage('');
+               })
             });
         },
         sendPreKeys: () => {
@@ -60,15 +83,13 @@ const Chat = () => {
                 window.libsignal.KeyHelper.generatePreKey(1).then(myPreKey => {
                     window.libsignal.KeyHelper.generatePreKey(2).then(myPreKey2 => {
                         window.libsignal.KeyHelper.generateSignedPreKey(myIdentityKeyPair, 100).then(mySignedPreKey => {
-                            const myRegistrationId = 1;
+                            const myRegistrationId = 1; //TODO
 
                             myProtocolStore = new SignalProtocolStore();
                             myProtocolStore.put('identityKey', myIdentityKeyPair);
                             myProtocolStore.put('registrationId', myRegistrationId);
                             myProtocolStore.storePreKey(myPreKey.keyId, myPreKey).then(() => {
                                myProtocolStore.storeSignedPreKey(mySignedPreKey.keyId, mySignedPreKey).then(() => {
-                                   console.log(myProtocolStore);
-
                                    let my_prekey_bundle_data = {
                                        identityKey: arraybuffer_to_string(myIdentityKeyPair.pubKey),
                                        preKeys: [
@@ -87,7 +108,7 @@ const Chat = () => {
                                            signature: arraybuffer_to_string(mySignedPreKey.signature)
                                        }
                                    };
-                                   post('pre_keys_bundle', my_prekey_bundle_data, () => console.log("Prekey bundle data sent!"));
+                                   post('pre_keys_bundle', my_prekey_bundle_data, () => console.log("Prekey sent!"));
                                });
                             });
                         });
@@ -114,7 +135,7 @@ const Chat = () => {
 
                 let sessionBuilder = new window.libsignal.SessionBuilder(myProtocolStore, receiverAddress);
                 sessionBuilder.processPreKey(receivedPreKeyBundle).then(() => {
-                    console.log("Przetworzono prekey!");
+                    console.log("Prekey processed!");
                 });
 
             });
@@ -126,9 +147,6 @@ const Chat = () => {
             actions.sendMessage();
         }
     }
-
-
-
 
     return (
         <div className='chat'>
